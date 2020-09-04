5f98afe074149208ba575a79b3f78624907b296c
James Jarvis <git@jamesjarvis.io>
2020-09-04 20:58:57 +0100

    :sparkles: Add user agent

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -1,37 +1,50 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
-    "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
 
 // This will get called for each HTML element found
 func processElement(index int, element *goquery.Selection) {
-    // See if the href attribute exists on the element
-    href, exists := element.Attr("href")
-    if exists {
-        fmt.Println(href)
-    }
+	// See if the href attribute exists on the element
+	href, exists := element.Attr("href")
+	if exists {
+		fmt.Println(href)
+	}
 }
 
 func main() {
-    // Make HTTP request
-    response, err := http.Get("https://www.jamesjarvis.io")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer response.Body.Close()
+	// Create HTTP client with timeout
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
 
-    // Create a goquery document from the HTTP response
-    document, err := goquery.NewDocumentFromReader(response.Body)
-    if err != nil {
-        log.Fatal("Error loading HTTP response body. ", err)
-    }
+	// Create and modify HTTP request before sending
+	request, err := http.NewRequest("GET", "https://www.jamesjarvis.io", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	request.Header.Set("User-Agent", "WebGraph v0.1 https://github.com/jamesjarvis/web-graph - This bot just follows links ¯\\_(ツ)_/¯")
 
-    // Find all links and process them with the function
-    // defined earlier
-    document.Find("a").Each(processElement)
+	// Make request
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	// Create a goquery document from the HTTP response
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
+	}
+
+	// Find all links and process them with the function
+	// defined earlier
+	document.Find("a").Each(processElement)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/jamesjarvis/web-graph
 
 go 1.15
 
-require github.com/PuerkitoBio/goquery v1.5.1 // indirect
+require github.com/PuerkitoBio/goquery v1.5.1
