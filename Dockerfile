e4350aef13999c55c2851cc8f653db4e96fc4067
James Jarvis <git@jamesjarvis.io>
2020-09-05 09:28:51 +0100

    :sparkles: Store cache in postgres

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Start from the latest golang base image
+FROM golang:latest as builder
+
+LABEL maintainer="James Jarvis <git@jamesjarvis.io>"
+
+RUN mkdir /build 
+WORKDIR /build
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+# Copy the source from the current directory to the Working Directory inside the container
+COPY . .
+
+# Build the Go app
+RUN CGO_ENABLED=0 GOOS=linux go build -o crawler cmd/crawler/main.go
+
+
+######## Start a new stage from scratch #######
+FROM alpine
+
+RUN adduser -S -D -H -h /app appuser
+USER appuser
+
+COPY --from=builder /build/crawler /app/
+WORKDIR /app
+CMD ["./crawler"]
--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/url"
+	"os"
 	"time"
 
 	"github.com/gocolly/colly"
+	"github.com/zolamk/colly-postgres-storage/colly/postgres"
 )
 
 func main() {
@@ -14,6 +17,17 @@ func main() {
 		colly.UserAgent("WebGraph v0.1 https://github.com/jamesjarvis/web-graph - This bot just follows links ¯\\_(ツ)_/¯"),
 	)
 
+	storage := &postgres.Storage{
+		URI:          fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=disable", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), "database", os.Getenv("POSTGRES_DB")),
+		VisitedTable: "colly_visited",
+		CookiesTable: "colly_cookies",
+	}
+	log.Print(storage.URI)
+
+	if err := c.SetStorage(storage); err != nil {
+		log.Fatal(err)
+	}
+
 	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 2, RandomDelay: 5 * time.Second})
 
 	// Find and visit all links
@@ -21,14 +35,14 @@ func main() {
 		link := e.Attr("href")
 		u, err := url.Parse(link)
 		if err != nil {
-			fmt.Println("bad url")
+			log.Println("bad url")
 		} else {
-			fmt.Println(e.Request.URL.Host + " --> " + u.Host)
+			log.Println(e.Request.URL.Host + " --> " + u.Host)
 			e.Request.Visit(link)
 		}
 	})
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+		log.Println("Visiting", r.URL)
 	})
 
 	c.Visit("https://www.jamesjarvis.io")
